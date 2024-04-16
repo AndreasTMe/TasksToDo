0b73feb71c0025564b69c1a3e5c8787346cccd6c
Andreas T <atsimpan@gmail.com>
2024-04-17 00:00:55 +0100

    Learning jetpack compose

--- app/src/main/java/com/andreast/taskstodo/GridViewItem.kt
+++
@@ -1,5 +0,0 @@
-package com.andreast.taskstodo
-
-data class GridViewItem(
-    val name: String
-)
--- app/src/main/java/com/andreast/taskstodo/MainActivity.kt
+++ app/src/main/java/com/andreast/taskstodo/MainActivity.kt
@@ -1,34 +1,78 @@
 package com.andreast.taskstodo
 
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.GridView
-import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.andreast.taskstodo.ui.theme.TasksToDoTheme
 
 class MainActivity : ComponentActivity() {
-    private lateinit var gridView: GridView
-    private lateinit var gridViewItems: List<GridViewItem>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.todo_gridview)
-
-        gridView = findViewById(R.id.idGridView)
-        gridViewItems = ArrayList()
-
-        for (i in 1..10) {
-            gridViewItems = gridViewItems + GridViewItem("Item $i")
+        setContent {
+            TasksToDoTheme {
+                TemporaryScreen()
+            }
         }
+    }
+}
 
-        val adapter = GridViewAdapter(gridViewItems, this@MainActivity)
-        gridView.adapter = adapter
-
-        gridView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            Toast.makeText(
-                applicationContext, gridViewItems[position].name + " selected",
-                Toast.LENGTH_SHORT
-            ).show()
+@Preview(showBackground = true)
+@Composable
+fun TemporaryScreen() {
+    Box(modifier = Modifier.fillMaxHeight()) {
+        LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+            items(10) {
+                Column(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .border(
+                            width = 1.dp,
+                            color = Color.Black,
+                            shape = MaterialTheme.shapes.small
+                        )
+                        .background(
+                            color = MaterialTheme.colorScheme.primary,
+                            shape = MaterialTheme.shapes.small
+                        )
+                        .aspectRatio(1f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .height(50.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "Item $it",
+                            color = Color.White,
+                        )
+                    }
+                }
+            }
         }
     }
 }
