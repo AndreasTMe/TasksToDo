136aaf095ac6129980edfcbae93191131939cdc9
Andreas T <atsimpan@gmail.com>
2024-04-17 00:32:19 +0100

    Added main grid view

--- app/src/main/java/com/andreast/taskstodo/MainActivity.kt
+++ app/src/main/java/com/andreast/taskstodo/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.andreast.taskstodo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -16,14 +18,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.andreast.taskstodo.ui.theme.TasksToDoTheme
@@ -33,7 +41,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TasksToDoTheme {
-                TemporaryScreen()
+                TodoGrid()
             }
         }
     }
@@ -41,38 +49,72 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun TemporaryScreen() {
-    Box(modifier = Modifier.fillMaxHeight()) {
-        LazyVerticalGrid(columns = GridCells.Fixed(3)) {
-            items(10) {
-                Column(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .border(
-                            width = 1.dp,
-                            color = Color.Black,
-                            shape = MaterialTheme.shapes.small
-                        )
-                        .background(
-                            color = MaterialTheme.colorScheme.primary,
-                            shape = MaterialTheme.shapes.small
-                        )
-                        .aspectRatio(1f),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .height(50.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = "Item $it",
-                            color = Color.White,
-                        )
+fun TodoGrid() {
+    var count by remember { mutableIntStateOf(0) }
+
+    Scaffold(
+        content = { padding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(padding)
+            ) {
+                LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+                    items(count) {
+                        Column(
+                            modifier = Modifier
+                                .aspectRatio(1f)
+                                .padding(8.dp)
+                                .border(
+                                    width = 2.dp,
+                                    color = Color.Gray,
+                                    shape = MaterialTheme.shapes.small
+                                )
+                                .background(
+                                    color = MaterialTheme.colorScheme.surface,
+                                    shape = MaterialTheme.shapes.small
+                                )
+                                .clickable {
+                                    Log.d("TemporaryScreen", "Item $it clicked")
+                                },
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .height(50.dp),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(
+                                    text = "Item $it",
+                                    color = MaterialTheme.colorScheme.inverseSurface,
+                                )
+                            }
+                        }
                     }
                 }
             }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = MaterialTheme.colorScheme.primary,
+                content = {
+                    Icon(Icons.Filled.Add, contentDescription = "Add Task")
+                },
+                onClick = {
+                    count++
+                }
+            )
+        },
+        bottomBar = {
+            Box(
+                modifier = Modifier
+                    .height(56.dp)
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.primary)
+            ) {
+            }
         }
-    }
+    )
 }
