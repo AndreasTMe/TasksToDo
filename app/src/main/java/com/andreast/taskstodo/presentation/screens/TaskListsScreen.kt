b101e738f4f52bd0aff47905a8fba0fd15f3331c
Andreas T <atsimpan@gmail.com>
2024-04-30 23:17:20 +0100

    Basic drag-n-drop for task lists

--- app/src/main/java/com/andreast/taskstodo/presentation/screens/TaskListsScreen.kt
+++ app/src/main/java/com/andreast/taskstodo/presentation/screens/TaskListsScreen.kt
@@ -1,5 +1,6 @@
 package com.andreast.taskstodo.presentation.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -9,8 +10,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -23,19 +26,25 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.andreast.taskstodo.presentation.components.InputDialog
 import com.andreast.taskstodo.presentation.components.tasks.TaskListsScreenTopHeader
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TaskListsScreen(
     taskListsScreenViewModel: TaskListsScreenViewModel,
@@ -62,11 +71,21 @@ fun TaskListsScreen(
                     .fillMaxHeight()
                     .padding(padding)
             ) {
-                LazyVerticalGrid(columns = GridCells.Fixed(3)) {
-                    items(taskListsScreenState.value.lists.size) {
+                LazyVerticalGrid(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    columns = GridCells.Fixed(3)
+                ) {
+                    items(taskListsScreenState.value.lists.size) { index ->
+                        var offsetX by remember { mutableFloatStateOf(0f) }
+                        var offsetY by remember { mutableFloatStateOf(0f) }
+
                         Column(
                             modifier = Modifier
                                 .aspectRatio(1f)
+                                .offset {
+                                    IntOffset(offsetX.roundToInt(), offsetY.roundToInt())
+                                }
                                 .padding(8.dp)
                                 .border(
                                     width = 2.dp,
@@ -79,24 +98,29 @@ fun TaskListsScreen(
                                 )
                                 .clickable {
                                     navHostController.navigate(
-                                        route = Screen.TaskListScreen.createRoute(taskListId = taskListsScreenState.value.lists[it].id.toString())
+                                        route = Screen.TaskListScreen.createRoute(
+                                            taskListId = taskListsScreenState.value.lists[index].id.toString()
+                                        )
                                     )
                                 }
                                 .pointerInput(Unit) {
                                     detectDragGesturesAfterLongPress(
-                                        onDrag = { change, offset ->
-
-                                        },
-                                        onDragStart = { offset ->
+                                        onDragStart = {
 
                                         },
                                         onDragEnd = {
-
+                                            offsetX = 0f
+                                            offsetY = 0f
                                         },
                                         onDragCancel = {
-
+                                            offsetX = 0f
+                                            offsetY = 0f
                                         }
-                                    )
+                                    ) { change, dragAmount ->
+                                        change.consume()
+                                        offsetX += dragAmount.x
+                                        offsetY += dragAmount.y
+                                    }
                                 },
                             horizontalAlignment = Alignment.CenterHorizontally,
                             verticalArrangement = Arrangement.Center
@@ -108,7 +132,7 @@ fun TaskListsScreen(
                                 contentAlignment = Alignment.Center
                             ) {
                                 Text(
-                                    text = taskListsScreenState.value.lists[it].title,
+                                    text = taskListsScreenState.value.lists[index].title,
                                     color = MaterialTheme.colorScheme.inverseSurface,
                                 )
                             }
