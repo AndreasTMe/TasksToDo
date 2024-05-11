3fd3f6e654387fe51a9cfff0ef75d585d8e70173
Andreas T <atsimpan@gmail.com>
2024-05-11 11:46:31 +0100

    Reordering when from less than to done

--- app/src/main/java/com/andreast/taskstodo/application/services/impl/TaskOrderingService.kt
+++ app/src/main/java/com/andreast/taskstodo/application/services/impl/TaskOrderingService.kt
@@ -1,5 +1,6 @@
 package com.andreast.taskstodo.application.services.impl
 
+import com.andreast.taskstodo.application.dto.Level
 import com.andreast.taskstodo.application.dto.TaskListItemDto
 import com.andreast.taskstodo.application.services.ITaskOrderingService
 
@@ -24,7 +25,55 @@ class TaskOrderingService : ITaskOrderingService {
         to: Int,
         items: List<TaskListItemDto>
     ): List<TaskListItemDto> {
-        // TODO(Implement)
-        return emptyList()
+        if (from == to || from !in items.indices || to !in items.indices) {
+            return emptyList()
+        }
+
+        val fromItem = items[from]
+        val toItem = items[to]
+
+        if (from < to) {
+            var deepestLevel = items[from].level
+            var index = from + 1
+            while (items[index].level > fromItem.level) {
+                if (items[index].level > deepestLevel) {
+                    deepestLevel = items[index].level
+
+                    if (deepestLevel + toItem.level > Level.entries.max()) {
+                        return emptyList()
+                    }
+                }
+
+                index++
+
+                if (index > to) {
+                    return emptyList()
+                }
+            }
+
+            if (to < items.size - 1 && items[to + 1].level > toItem.level) {
+                return items
+                    .filter { item ->
+                        item.parentId == toItem.id
+                    }
+                    .map { item ->
+                        item.copy(order = item.order + 1)
+                    }
+                    .plus(fromItem.copy(parentId = toItem.id, order = 0))
+            }
+
+            return items
+                .filter { item ->
+                    item.parentId == toItem.parentId && item.order > toItem.order
+                }
+                .map { item ->
+                    item.copy(order = item.order + 1)
+                }
+                .plus(fromItem.copy(parentId = toItem.parentId, order = toItem.order + 1))
+        } else {
+            // TODO(Implement "reorderTasks")
+
+            return emptyList()
+        }
     }
 }
