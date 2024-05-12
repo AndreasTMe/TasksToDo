c18acd75cef255a16937adea3f705036abe3140f
Andreas T <atsimpan@gmail.com>
2024-05-12 17:09:59 +0100

    Refactoring TaskOrderingService

--- app/src/main/java/com/andreast/taskstodo/application/services/impl/TaskOrderingService.kt
+++ app/src/main/java/com/andreast/taskstodo/application/services/impl/TaskOrderingService.kt
@@ -38,70 +38,40 @@ class TaskOrderingService : ITaskOrderingService {
 
         val fromItem = items[from]
         val toItem = items[to]
+        val shouldBecomeChild = checkIfShouldBecomeChild(from, to, items)
 
         if (from < to) {
-            val shouldBecomeChild = to < items.size - 1 && items[to + 1].level > toItem.level
-
             if (canReorderWhenMovingDown(from, to, items, shouldBecomeChild)) {
-                if (shouldBecomeChild) {
-                    var fromLevelOrder = 0
-                    var toLevelOrder = 1
-
-                    return items
-                        .filter { item ->
-                            item.parentId == fromItem.parentId || item.parentId == toItem.id
-                        }
-                        .sortedBy { it.order }
-                        .map {
-                            if (it.id == fromItem.id) {
-                                return@map it.copy(parentId = toItem.id, order = 0)
-                            } else if (it.parentId == fromItem.parentId) {
-                                return@map it.copy(order = fromLevelOrder++)
-                            } else {
-                                return@map it.copy(order = toLevelOrder++)
-                            }
-                        }
+                return if (shouldBecomeChild) {
+                    items.getReorderedWhenMovingDownAsChild(fromItem, toItem)
+                } else {
+                    items.getReorderedWhenMovingDown(fromItem, toItem)
                 }
-
-                return items
-                    .filter { item ->
-                        item.parentId == toItem.parentId
-                    }
-                    .sortedBy { it.order }
-                    .toMutableList()
-                    .apply {
-                        val insertionIndex = indexOfFirst { it.id == toItem.id } + 1
-
-                        if (fromItem.parentId == toItem.parentId) {
-                            add(insertionIndex, fromItem)
-                            removeAt(indexOfFirst { it.id == fromItem.id })
-                        } else {
-                            add(insertionIndex, fromItem.copy(parentId = toItem.parentId))
-                        }
-                    }
-                    .mapIndexed { i, item -> item.copy(order = i) }
             }
         } else if (from > to) {
-            // TODO(Implement "reorderTasks")
-
-            var currentDeepestLevel = items[from].level
-            var index = from + 1
-            while (index < items.size && items[index].level > fromItem.level) {
-                if (items[index].level > currentDeepestLevel) {
-                    currentDeepestLevel = items[index].level
-
-                    if (Level.addValues(currentDeepestLevel, toItem.level) > Level.maxValue()) {
-                        return emptyList()
-                    }
+            if (canReorderWhenMovingUp(from, to, items, shouldBecomeChild)) {
+                // TODO(Implement "reorderTasks")
+                if (shouldBecomeChild) {
+                    return emptyList()
                 }
 
-                index++
+                return emptyList()
             }
-
-            return emptyList()
         }
 
         return emptyList()
+    }
+
+    private fun checkIfShouldBecomeChild(
+        from: Int,
+        to: Int,
+        items: List<TaskListItemDto>
+    ): Boolean {
+        return if (from < to) {
+            to < items.size - 1 && items[to + 1].level > items[to].level
+        } else {
+            to > 0 && items[to].level > items[to - 1].level
+        }
     }
 
     internal fun canReorderWhenMovingDown(
@@ -132,4 +102,73 @@ class TaskOrderingService : ITaskOrderingService {
 
         return true
     }
+
+    internal fun canReorderWhenMovingUp(
+        from: Int,
+        to: Int,
+        items: List<TaskListItemDto>,
+        shouldBecomeChild: Boolean
+    ): Boolean {
+        var currentDeepestLevel = items[from].level
+        var index = from + 1
+        while (index < items.size && items[index].level > items[from].level) {
+            if (items[index].level > currentDeepestLevel) {
+                currentDeepestLevel = items[index].level
+
+                if (Level.addValues(currentDeepestLevel, items[to].level) > Level.maxValue()) {
+                    return false
+                }
+            }
+
+            index++
+        }
+
+        return true
+    }
+}
+
+private inline fun List<TaskListItemDto>.filterSorted(predicate: (TaskListItemDto) -> Boolean): List<TaskListItemDto> {
+    return filter(predicate).sortedBy { it.order }
+}
+
+private fun List<TaskListItemDto>.getReorderedWhenMovingDown(
+    fromItem: TaskListItemDto,
+    toItem: TaskListItemDto
+): List<TaskListItemDto> {
+    return filterSorted { item ->
+        item.parentId == toItem.parentId
+    }
+        .toMutableList()
+        .apply {
+            val insertionIndex = indexOfFirst { it.id == toItem.id } + 1
+
+            if (fromItem.parentId == toItem.parentId) {
+                add(insertionIndex, fromItem)
+                removeAt(indexOfFirst { it.id == fromItem.id })
+            } else {
+                add(insertionIndex, fromItem.copy(parentId = toItem.parentId))
+            }
+        }
+        .mapIndexed { i, item -> item.copy(order = i) }
+}
+
+private fun List<TaskListItemDto>.getReorderedWhenMovingDownAsChild(
+    fromItem: TaskListItemDto,
+    toItem: TaskListItemDto
+): List<TaskListItemDto> {
+    var fromLevelOrder = 0
+    var toLevelOrder = 1
+
+    return filterSorted { item ->
+        item.parentId == fromItem.parentId || item.parentId == toItem.id
+    }
+        .map {
+            if (it.id == fromItem.id) {
+                return@map it.copy(parentId = toItem.id, order = 0)
+            } else if (it.parentId == fromItem.parentId) {
+                return@map it.copy(order = fromLevelOrder++)
+            } else {
+                return@map it.copy(order = toLevelOrder++)
+            }
+        }
 }
